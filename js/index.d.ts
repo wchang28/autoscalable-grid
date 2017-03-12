607b1747c2fc6b4610f6dda95d4c7971955b5c12
wchang28 <wchang28@hotmail.com>
2017-03-12 13:45:52 -0400

    gfbggf

--- js/index.d.ts
+++ js/index.d.ts
@@ -22,3 +22,41 @@ export interface IAutoScalableGrid {
     disableWorkers: (workerIds: string[]) => Promise<any>;
     setWorkersTerminating: (workerIds: string[]) => Promise<any>;
 }
+export declare type WorkerKey = string;
+export interface IWorkersLaunchRequest {
+    NumInstances: number;
+    Hint?: any;
+}
+export interface IAutoScalerImplementation {
+    TranslateToWorkerKeys: (workers: IWorker[]) => Promise<WorkerKey[]>;
+    EstimateWorkersLaunchRequest: (state: IAutoScalableState) => Promise<IWorkersLaunchRequest>;
+    LaunchInstances: (launchRequest: IWorkersLaunchRequest) => Promise<WorkerKey[]>;
+    TerminateInstances: (workerKeys: WorkerKey[]) => Promise<WorkerKey[]>;
+    getConfigUrl: () => Promise<string>;
+}
+export interface IGridAutoScalerJSON {
+    ScalingUp: boolean;
+    Enabled: boolean;
+    HasMaxWorkersCap: boolean;
+    MaxWorkersCap: number;
+    HasMinWorkersCap: boolean;
+    MinWorkersCap: number;
+    LaunchingWorkers: WorkerKey[];
+}
+export interface IGridAutoScaler {
+    isScalingUp: () => Promise<boolean>;
+    launchNewWorkers: (launchRequest: IWorkersLaunchRequest) => Promise<boolean>;
+    terminateWorkers: (workers: IWorker[]) => Promise<boolean>;
+    isEnabled: () => Promise<boolean>;
+    enable: () => Promise<any>;
+    disable: () => Promise<any>;
+    hasMaxWorkersCap: () => Promise<boolean>;
+    hasMixWorkersCap: () => Promise<boolean>;
+    getMaxWorkersCap: () => Promise<number>;
+    setMaxWorkersCap: (value: number) => Promise<number>;
+    getMinWorkersCap: () => Promise<number>;
+    setMinWorkersCap: (value: number) => Promise<number>;
+    getLaunchingWorkers: () => Promise<WorkerKey[]>;
+    getJSON: () => Promise<IGridAutoScalerJSON>;
+    getImplementationConfigUrl: () => Promise<string>;
+}
--- ts/index.ts
+++ ts/index.ts
@@ -24,3 +24,46 @@ export interface IAutoScalableGrid {
     disableWorkers: (workerIds: string[]) => Promise<any>;
     setWorkersTerminating: (workerIds: string[]) => Promise<any>;
 }
+
+export type WorkerKey = string; // worker key used to terminate/launch worker, actual implementation decide what this is
+
+export interface IWorkersLaunchRequest {
+    NumInstances: number;
+    Hint?: any;
+}
+
+export interface IAutoScalerImplementation {
+    TranslateToWorkerKeys: (workers: IWorker[]) => Promise<WorkerKey[]>;     // translate from IWorker to WorkerKey
+    EstimateWorkersLaunchRequest: (state: IAutoScalableState) => Promise<IWorkersLaunchRequest>;  // calculate the number of additional workers desired given the current state of the autoscalable
+    LaunchInstances: (launchRequest: IWorkersLaunchRequest) => Promise<WorkerKey[]>;                // actual implementation of launching new workers
+    TerminateInstances: (workerKeys: WorkerKey[]) => Promise<WorkerKey[]>;                          // actual implementation of terminating the workers
+    getConfigUrl:  () => Promise<string>;                                                           // configuration url for the actual implementation
+}
+
+export interface IGridAutoScalerJSON {
+    ScalingUp: boolean;
+    Enabled: boolean;
+    HasMaxWorkersCap: boolean;
+    MaxWorkersCap: number;
+    HasMinWorkersCap: boolean;
+    MinWorkersCap: number;
+    LaunchingWorkers: WorkerKey[];
+}
+
+export interface IGridAutoScaler {
+    isScalingUp: () => Promise<boolean>;
+    launchNewWorkers: (launchRequest: IWorkersLaunchRequest) => Promise<boolean>;
+    terminateWorkers: (workers: IWorker[]) => Promise<boolean>;
+    isEnabled: () => Promise<boolean>;
+    enable: () => Promise<any>;
+    disable: () => Promise<any>;
+    hasMaxWorkersCap: () => Promise<boolean>;
+    hasMixWorkersCap: () => Promise<boolean>;
+    getMaxWorkersCap: () => Promise<number>;
+    setMaxWorkersCap: (value: number) => Promise<number>;
+    getMinWorkersCap: () => Promise<number>;
+    setMinWorkersCap: (value: number) => Promise<number>;
+    getLaunchingWorkers: () => Promise<WorkerKey[]>;
+    getJSON: () => Promise<IGridAutoScalerJSON>;
+    getImplementationConfigUrl: () => Promise<string>;
+}
